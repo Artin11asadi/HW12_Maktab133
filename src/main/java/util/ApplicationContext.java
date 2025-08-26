1492f481f651dc4aefab2829b2d5e6eaa5e45d9c
Artin11asadi <aaturbo1381@gmail.com>
2025-08-26 17:13:16 +0330

    feat: ApplicationContext
    -Add made getTruckService and getCarService and our carDetachmentDemo methods
    
    -Add instantiated our repositories and our services

--- src/main/java/util/ApplicationContext.java
+++ src/main/java/util/ApplicationContext.java
@@ -1,16 +1,37 @@
-package util;//package util;
-//
-//public class ApplicationContext {
-//
-//    private static UserRepository userRepository;
-//    private static UserService userService;
-//
-//    static {
-//        userRepository=new UserRepositoryImp();
-//        userService=new UserServiceImpl(userRepository);
-//    }
-//
-//    public static UserService getUserService() {
-//        return userService;
-//    }
-//}
+package util;
+
+import model.Fuel;
+import repository.impl.CarRepositoryImpl;
+import repository.impl.TruckRepositoryImpl;
+import service.CarService;
+import service.TruckService;
+import service.impl.CarServiceImpl;
+import service.impl.TruckServiceImpl;
+
+public class ApplicationContext {
+
+    private static final TruckRepositoryImpl truckRepository;
+    private static final CarRepositoryImpl carRepository;
+    private static final TruckService truckService;
+    private static final CarService carService;
+
+
+    static {
+        truckRepository = new TruckRepositoryImpl();
+        carRepository = new CarRepositoryImpl();
+        truckService = new TruckServiceImpl(truckRepository);
+        carService = new CarServiceImpl(carRepository);
+    }
+
+    public static TruckService getTruckService() {
+        return truckService;
+    }
+
+    public static CarService getCarService(){
+        return carService;
+    }
+
+    public static void carDetachmentDemo(Long id, Fuel newFuelType){
+        carRepository.carDetachmentDemo(id, newFuelType);
+    }
+}
